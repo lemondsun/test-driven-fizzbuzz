c569b9c198f1ee51512b4fc97ae763367df18b1a
Jason Mullings <wjasonmullings@gmail.com>
2020-06-05 10:09:50 -0400

    :tada: full  coverage

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-module.exports = (num) => {
-  if (num % 15 === 0) return 'FizzBuzz';
-    return num
-   };
-
+module.exports = () => {
+  arr = []
+  for (i = 0; i <= 100; i++) {
+    if ( i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if ( i % 5 === 0) {
+      arr.push("Buzz");
+    }
+    else
+      arr.push(i)
+  }
+  return arr
+};
--- index.test.js
+++ index.test.js
@@ -3,12 +3,22 @@ const fizzBuzz = require("./index");
 
 describe("fizzBuzz()", () => {
   it('returns "FizzBuzz" for multiples of 3 and 5', () => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz");
-    expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz()[15]).toBe("FizzBuzz");
+    expect(fizzBuzz()[30]).toBe("FizzBuzz");
   });
 
+  it('returns "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz()[3]).toBe("Fizz");
+    expect(fizzBuzz()[9]).toBe("Fizz");
+  })
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz()[5]).toBe("Buzz");
+    expect(fizzBuzz()[10]).toBe("Buzz");
+  })
+
   it('returns the number if number is not a multiple of 3 or 5', () => {
-    expect(fizzBuzz(1)).toBe(1);
-    expect(fizzBuzz(2)).toBe(2);
+    expect(fizzBuzz()[1]).toBe(1);
+    expect(fizzBuzz()[2]).toBe(2);
   });
 });
